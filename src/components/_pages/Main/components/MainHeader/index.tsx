3efd25e721a28b60619b4b3e566a71f160792d16
Regis Bafutwabo <>
2022-03-28 01:31:26 +0900

    fix: profile size

--- src/components/_pages/Main/components/MainHeader/index.tsx
+++ src/components/_pages/Main/components/MainHeader/index.tsx
@@ -3,7 +3,7 @@ import { Box, Img } from '@chakra-ui/react';
 export const MainHeader = () => {
   return (
     <Box>
-      <Img borderRadius="full" objectFit="cover" boxSize={['100px', '200px', '200px', '200px']} src="/profile.jpeg" alt="Regis" />
+      <Img borderRadius="full" objectFit="contain" boxSize={['100px', '200px', '200px', '200px']} src="/profile.jpeg" alt="Regis" />
     </Box>
   );
 };
