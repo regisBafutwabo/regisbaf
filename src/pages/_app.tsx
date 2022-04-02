11e2c5bce57e983a070fa972abb23f5adf2cbab4
Regis Bafutwabo <>
2022-04-02 13:56:37 +0900

    fix: integrated a service worker for faster load

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -42,6 +42,14 @@ function MyApp({ Component, pageProps }: AppProps) {
     router.events.on('routeChangeComplete', routeChangeComplete);
   }, [router.events]);
 
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js');
+      });
+    }
+  }, []);
+
   return (
     <ChakraProvider theme={updatedTheme}>
       <AppLayout>
