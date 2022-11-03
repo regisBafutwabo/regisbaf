42b1b7118a772a93a1866578b2d1d5ca9327fecd
regisBafutwabo <regiswareja@gmail.com>
2022-11-03 18:38:01 +0900

    feat: added getStaticProps for all the the blog slug page

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,7 +1,43 @@
+import { SANITY_API_VERSION, SANITY_DATA_SETS, SANITY_ID } from 'constants/env';
 import { NextPage } from 'next';
+import { createClient } from 'next-sanity';
 
-const BlogSlug: NextPage = () => {
+const BlogSlug: NextPage = (props) => {
   return <></>;
 };
+
+export async function getStaticPaths() {
+  const client = createClient({
+    projectId: SANITY_ID,
+    dataset: SANITY_DATA_SETS,
+    apiVersion: SANITY_API_VERSION,
+    useCdn: false,
+  });
+
+  const posts = await client.fetch(`*[_type == "post"]`);
+
+  return {
+    paths: posts.map((p: any) => ({ params: { slug: `${p?.slug?.current}` } })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: any) {
+  const client = createClient({
+    projectId: SANITY_ID,
+    dataset: SANITY_DATA_SETS,
+    apiVersion: SANITY_API_VERSION,
+    useCdn: false,
+  });
+
+  const posts = await client.fetch(
+    `*[_type == "post" && slug.current=="${params.slug}"]`
+  );
+  return {
+    props: {
+      post: posts,
+    },
+  };
+}
 
 export default BlogSlug;
