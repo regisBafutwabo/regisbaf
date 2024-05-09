bd1be308c95c672fb439b56760b3a856d3fec63d
regisBafutwabo <regiswareja@gmail.com>
2024-05-09 09:57:39 +0900

    feat: fixed footer styling

--- src/components/AppLayout/components/Footer/index.tsx
+++ src/components/AppLayout/components/Footer/index.tsx
@@ -1,15 +1,27 @@
+import { Container } from '@chakra-ui/react';
+
 import CurrentlyPlaying from './components/CurrentlyPlaying';
 import { FooterLinks } from './components/FooterLinks';
 
 export const Footer = () => {
   return (
-    <footer style={{
-      paddingTop: 8,
-      borderTopColor: 'gray.700',
-      borderTopWidth: '1px',
-    }}>
-      <CurrentlyPlaying />
-      <FooterLinks />
+    <footer
+      style={{
+        paddingTop: 8,
+        // marginRight: 'auto',
+        // marginLeft: 'auto',
+        // display: 'flex',
+        // flexDirection: 'column',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        borderTopColor: 'gray[700]',
+        borderTopWidth: '1px',
+      }}
+    >
+      <Container maxW="container.lg">
+        <CurrentlyPlaying />
+        <FooterLinks />
+      </Container>
     </footer>
   );
 };
